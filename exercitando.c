2d884671af4cdc7f5a087843d8e59c5ff508d333
João Inacio <joaoinacio0612@gmail.com>
2026-03-12 11:41:11 -0300

    Update exercitando.c

--- exercitando.c
+++ exercitando.c
@@ -1,11 +1,6 @@
 #include <stdio.h.> 
 int main() {
-    int numero = 80;
-    float numero2 = 3.6;
-    double numero3 = 3.14569;
-    printf("Numero: %d, %f, %f", numero, numero2, numero3);
+    printf("Olá mundo"); 
 
-
-    return 0; 
-
+    return 0;
 }
