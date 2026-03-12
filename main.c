bc225c467328cf73b3b4783e3d569bd195e2acde
joao inacio <joaoinacio0612@gmail.com>
2026-03-12 11:28:30 -0300

    aprendendo c

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+// biblioteca para impressão
+int main() {
+    // main = função principal
+    int numero = 80;
+    float numero2 = 3.6;
+    double numero3 = 3.14569;
+    // declarando variáveis
+    printf("Numero: %d, %f, %f", numero, numero2, numero3);
+    
+    // imprimindo variáveis
+    return 0;
+}
+// tipos de dados: int, char, float, double 
+// int: inteiros, char: caracteres, float: ponto flutuante, double: ponto flutuante com precisão maior
