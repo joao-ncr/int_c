a07426235644ffcf7c85aea59289de433d5089cd
joao inacio <joaoinacio0612@gmail.com>
2026-03-12 12:24:07 -0300

    finalizando

---
+++ dia_01/receba.c
@@ -0,0 +1,10 @@
+// scanf = input no python 
+#include <stdio.h> 
+int main() {
+    printf("Entre com um numero:");
+    int numero;
+    scanf("%i", &numero);
+    printf("O dobro do numero digitado foi: %d", numero * 2);
+    return 0;
+}
+
